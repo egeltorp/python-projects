70dd5f6429bffd18a8eb22914033ae57de986b3f
Theodor Holmberg <35803316+egeltorp@users.noreply.github.com>
2025-09-09 11:42:04 +0200

    nearly done

--- lab3-3.py
+++ lab3-3.py
@@ -1,12 +1,33 @@
-antal_tärningar = int(input("Hur många tärningar behövs i spelet? "))
-antal_kast = int(input("Hur många kast får en spelare? "))
+import random
 
 while True:
-    for i in range(antal_kast):
-        if i == 0:
-            q = input("Genom att trycka på enter kan du börja kasta, om du vill avsluta spelet skriv A: ")
-            if q == "A":
-                break
+    antal_tärningar = int(input("Hur många tärningar behövs i spelet? "))
+    max_kast = int(input("Hur många kast får en spelare? "))
+
+    start = input("Klicka ENTER för att starta, skriv A för att avsluta: ")
+    if start.upper() == "A":
+        break
+
+    for i in range(max_kast):
+        print(f"\nKast {i+1}:")
+        for j in range(antal_tärningar):
+            kast_värde = random.randint(1, 6)
+            print(f">Tärning {j+1}: {kast_värde}")
+
+        # antal kast kvar, efter första omgången
+        print(f"\nNu har du {max_kast - (i+1)} kast kvar.")
         
-        
-        
+        # fråga om en omgång till, eller avsluta
+        kast_kvar = max_kast - (i+1)
+        if kast_kvar == 0:  
+            igen = input("Vill du börja om? (Y/N): ")
+        else:
+            igen = input("Vill du kasta igen? (Y/N): ")
+
+        if igen.upper() != "Y":
+            print("Avslutar.")
+            exit()
+        elif kast_kvar == 0:
+            print()
+            break
+    
