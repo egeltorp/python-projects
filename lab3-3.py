c888f43011059163992c8d4a820fed291ff46274
Theodor Holmberg <35803316+egeltorp@users.noreply.github.com>
2025-09-09 11:58:35 +0200

    finish lab3-3.py, add ascii, add dice history

--- lab3-3.py
+++ lab3-3.py
@@ -1,18 +1,40 @@
 import random
 
+def display_ascii_art():
+    ascii_art = '''
+$$\                $$\        $$$$$$\  
+$$ |               $$ |      $$ ___$$\ 
+$$ |      $$$$$$\  $$$$$$$\  \_/   $$ |
+$$ |      \____$$\ $$  __$$\   $$$$$ / 
+$$ |      $$$$$$$ |$$ |  $$ |  \___$$\ 
+$$ |     $$  __$$ |$$ |  $$ |$$\   $$ |
+$$$$$$$$\\$$$$$$$ |$$$$$$$  |\$$$$$$  |
+\________|\_______|\_______/  \______/ 
+                                                                                        
+'''
+    print(ascii_art)
+
 while True:
+    display_ascii_art()
+
     antal_tärningar = int(input("Hur många tärningar behövs i spelet? "))
     max_kast = int(input("Hur många kast får en spelare? "))
 
     start = input("Klicka ENTER för att starta, skriv A för att avsluta: ")
     if start.upper() == "A":
+        print("Tack och hej.")
         break
 
     for i in range(max_kast):
         print(f"\nKast {i+1}:")
+        historik: list = []
         for j in range(antal_tärningar):
             kast_värde = random.randint(1, 6)
+            historik.append(kast_värde)
             print(f">Tärning {j+1}: {kast_värde}")
+
+        # berätta kast-värden
+        print(f"Du fick {', '.join(map(str, historik))}")
 
         # antal kast kvar, efter första omgången
         print(f"\nNu har du {max_kast - (i+1)} kast kvar.")
