a600af42abdf45d5aa4f7891921a4ff4fc14557b
Theodor Holmberg <35803316+egeltorp@users.noreply.github.com>
2025-09-15 14:19:05 +0200

    add print_chocolate_bar

--- lab4-1.py
+++ lab4-1.py
@@ -2,22 +2,30 @@
 # 2025-09-16
 # Theodor Holmberg
 
-# func to create the chocolate bar
+# func to create the chocolate bar matrix
 def create_chocolate_bar(rows: int, cols: int):
-    # empty matrix
-    chocolate = []
+    # error handling
+    if rows <= 0 or cols <= 0:
+        return None
 
+    # matrix
+    chocolate = []
     for r in range(1, rows + 1):
         row = []
         for c in range(1, cols + 1):
             row.append(f"{r}{c}")
         chocolate.append(row)
-    
     return chocolate
 
-# func to print the chocolate bar
-def print_chocolate_bar():
+# print chocolate bar in a formatted table
+def print_chocolate_bar(matrix):
+    for row in matrix:
+        print("  ".join(row))
+
+# func to 'chomp' away pieces of the chocolate bar
+def chomp(chocolate: bar, row: int, col: int):
     pass
 
-print(create_chocolate_bar(3,4))
+choco = create_chocolate_bar(5, 5)
+print_chocolate_bar(choco)
 
