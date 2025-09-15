511f84ef356171aae7d13faa2c3795ef75a9c59f
Theodor Holmberg <35803316+egeltorp@users.noreply.github.com>
2025-09-15 21:26:49 +0200

    change win check

--- lab4-2.py
+++ lab4-2.py
@@ -4,43 +4,46 @@
 
 # func to create the chocolate bar matrix
 def create_chocolate_bar(rows: int, cols: int):
-    # error handling
     if rows <= 0 or cols <= 0:
         return None
 
-    # matrix
     chocolate = []
     for r in range(1, rows + 1):
         row = []
         for c in range(1, cols + 1):
             row.append(f"{r}{c}")
         chocolate.append(row)
+
+    chocolate[0][0] = "P" # --> 'poison' 1st square
     return chocolate
 
-# print chocolate bar in a formatted table
+# print matrix in a formatted table
 def print_chocolate_bar(matrix):
     for row in matrix:
         print("  ".join(f"{col:>3}" for col in row))
 
 # func to 'chomp' away pieces of the chocolate bar
 def chomp(matrix: list, row: int, col: int):
-    # error handling
-    if row < 0 or cols < 0:
+    if row < 0 or col < 0:
         return None
 
     result = []
-
-    # reformat from user input to correct index
-    # row -= 1
-    # col -= 1
 
     # loops for each row, checks if row is before chomp -> 
     # if true: keeps that row intact, appends to result
     for r in range(len(matrix)):
         if r < row:
-            # rows before the chomp stay intact, save whole row
+            # save whole row
             result.append(matrix[r][:])
         else:
-            # rows at or below row get cut at col, save only up to col index
+            # save only up to col index
             result.append(matrix[r][:col])
     return result
+
+def check_winner(matrix: list):
+    return matrix == ["P"]
+
+def ask_cell_number(matrix: list):
+    pass
+
+print_chocolate_bar(create_chocolate_bar(3,4))
